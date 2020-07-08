a0a261564237493af2d814a38c08fdac91eadbe2
Liskuxx <lelea500@gmail.com>
2020-07-09 01:56:18 +1200

    Add in timer

--- Pong/src/PongPanel.java
+++ Pong/src/PongPanel.java
@@ -3,14 +3,19 @@ import java.awt.Color;
    import java.awt.event.ActionListener;
    import java.awt.event.KeyEvent;
    import java.awt.event.KeyListener;
-   import javax.swing.JPanel;
+
+import javax.management.timer.Timer;
+import javax.swing.JPanel;
    
    public class PongPanel extends JPanel implements ActionListener, KeyListener {
        
 	   private final static Color BACKGROUND_COLOUR = Color.BLACK;
+	   private final static int TIMER_DELAY = 5;
 	   
       public PongPanel() {
           setBackground(BACKGROUND_COLOUR);
+          Timer timer = new Timer(TIMER_DELAY, this);
+          timer.start();
       }
   
       @Override
