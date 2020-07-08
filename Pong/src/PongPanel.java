2d66b4292b1265d3ecdf12227437e07638171855
Liskuxx <lelea500@gmail.com>
2020-07-09 02:03:17 +1200

    First square drawn to game window

--- Pong/src/PongPanel.java
+++ Pong/src/PongPanel.java
@@ -5,6 +5,8 @@
    import java.awt.event.KeyListener;
    import javax.swing.Timer;
    import javax.swing.JPanel;
+   import java.awt.Graphics;
+   import java.awt.Graphics2D;
    
    public class PongPanel extends JPanel implements ActionListener, KeyListener {
        
@@ -16,19 +18,36 @@
           Timer timer = new Timer(TIMER_DELAY, this);
           timer.start();
       }
+      
+      //Loop controlling the game
+      private void update() {
+          
+      }
+      
+      //Paint the canvas to default state
+      @Override
+      public void paintComponent(Graphics g) {
+          super.paintComponent(g);
+          g.setColor(Color.WHITE);
+          g.fillRect(20, 20, 100, 100);
+      }
   
+      //Key pressed event
       @Override
       public void keyPressed(KeyEvent event) {
       }
   
+      //Key release event
       @Override
       public void keyReleased(KeyEvent event) {
       }
   
+      //Key typed event
       @Override
       public void keyTyped(KeyEvent event) {
       }
   
+      //Action performed event
       @Override
       public void actionPerformed(ActionEvent event) {
       }
