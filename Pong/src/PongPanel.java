6b2faec5a7bc9a816812cb08fa377192a715cfa0
Liskuxx <lelea500@gmail.com>
2020-07-09 01:57:43 +1200

    Fix to import timer library

--- Pong/src/PongPanel.java
+++ Pong/src/PongPanel.java
@@ -1,11 +1,10 @@
-import java.awt.Color;
+	import java.awt.Color;
    import java.awt.event.ActionEvent;
    import java.awt.event.ActionListener;
    import java.awt.event.KeyEvent;
    import java.awt.event.KeyListener;
-
-import javax.management.timer.Timer;
-import javax.swing.JPanel;
+   import javax.management.timer.Timer;
+   import javax.swing.JPanel;
    
    public class PongPanel extends JPanel implements ActionListener, KeyListener {
        
