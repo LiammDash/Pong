e3e2b97733246c16e638f6bb9b1a8f0f8d891fd4
Liskuxx <lelea500@gmail.com>
2020-07-10 03:36:08 +1200

    Sprites are finally being drawn

--- Pong/src/PongPanel.java
+++ Pong/src/PongPanel.java
@@ -12,19 +12,24 @@
    import java.awt.Graphics2D;
    
    public class PongPanel extends JPanel implements ActionListener, KeyListener {
-	   boolean gameInitialised = false;
-	   
+	      
 	   
        //Final variables
 	   private final static Color BACKGROUND_COLOUR = Color.BLACK;
 	   private final static int TIMER_DELAY = 5;
 	  
-	  //Create ball object
+	  //Create objects
 	  Ball ball;
+	  Paddle paddle1, paddle2;
 	  
-	  //Create a new ball for the game
+	  //Create GameState variable
+	  GameState gameState = GameState.initialising;
+	  
+	  //Create objects
 	  public void createObjects() {
-	         ball = new Ball(getWidth(), getHeight());
+		  ball = new Ball(getWidth(), getHeight());
+          paddle1 = new Paddle(Player.One, getWidth(), getHeight());
+          paddle2 = new Paddle(Player.Two, getWidth(), getHeight());
 	  }
 	  
 	  
@@ -38,20 +43,33 @@
       //Loop controlling the game
       private void update() {
     	  //Create objects and then start game running
-    	  if(!gameInitialised) {
-              createObjects();
-              gameInitialised = true;
-         }
+    	  switch(gameState) {
+    	  case initialising : {
+    		  createObjects();
+    		  System.out.println("Created objects");
+    		  gameState = GameState.playing;
+    		  break;
+    	  }
+    	  case playing: {
+    		  break;
+    	  }
+    	  case gameOver: {
+    		  break;
+    	  }
+    	  }
       }
       
-      //Paint the canvas to default state
+      //Paint components
       @Override
       public void paintComponent(Graphics g) {
-    	  if(gameInitialised) {
+    	  super.paintComponent(g);
+          paintDottedLine(g);
+    	  if(gameState != GameState.initialising) {
               paintSprite(g, ball);
+              paintSprite(g, paddle1);
+              paintSprite(g, paddle2);
           }
-          super.paintComponent(g);
-          paintDottedLine(g);          
+                    
       }
       
       //Paint sprite method
@@ -59,6 +77,16 @@
           g.setColor(sprite.getColor());
           g.fillRect(sprite.getXPosition(), sprite.getYPosition(), sprite.getWidth(), sprite.getHeight());
       }
+      
+    //Draw divider
+      private void paintDottedLine(Graphics g) {
+          Graphics2D g2d = (Graphics2D) g.create();
+             Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0);
+             g2d.setStroke(dashed);
+             g2d.setPaint(Color.WHITE);
+             g2d.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight());
+             g2d.dispose();
+     }
   
       //Key pressed event
       @Override
@@ -81,15 +109,5 @@
     	  update();
     	  repaint();
       }
-      
-      //Draw divider
-      private void paintDottedLine(Graphics g) {
-          Graphics2D g2d = (Graphics2D) g.create();
-             Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0);
-             g2d.setStroke(dashed);
-             g2d.setPaint(Color.WHITE);
-             g2d.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight());
-             g2d.dispose();
-     }
   
  }
